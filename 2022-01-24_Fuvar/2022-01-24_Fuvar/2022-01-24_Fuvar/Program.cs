de0a5eec3e87645f7df0d713bdda564d7e39b45b
kispols <kispal.attila0106@gmail.com>
2022-01-24 14:14:38 +0100

    4. feladat

--- 2022-01-24_Fuvar/2022-01-24_Fuvar/2022-01-24_Fuvar/Program.cs
+++ 2022-01-24_Fuvar/2022-01-24_Fuvar/2022-01-24_Fuvar/Program.cs
@@ -17,6 +17,18 @@ namespace _2022_01_24_Fuvar
                 fuvarok.Add(new Fuvar(sor));
             }
             Console.WriteLine($"3. feladat: {fuvarok.Count} fuvar");
+
+            double Bevétel = 0;
+            int db = 0;
+            foreach (var f in fuvarok)
+            {
+                if (f.TaxiID == 6185)
+                {
+                    Bevétel += f.Viteldíj + f.Borravaló;
+                    db++;
+                }
+            }
+            Console.WriteLine($"4.Feladat: {db} fuvar alatt: {Bevétel} $");
         }
     }
 }
