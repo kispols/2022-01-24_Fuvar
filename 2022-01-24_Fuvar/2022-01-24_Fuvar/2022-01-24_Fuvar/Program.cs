bfa31825fef1681b59307df1b06954447c728865
kispols <kispal.attila0106@gmail.com>
2022-01-24 14:08:45 +0100

    3.feladat

--- 2022-01-24_Fuvar/2022-01-24_Fuvar/2022-01-24_Fuvar/Program.cs
+++ 2022-01-24_Fuvar/2022-01-24_Fuvar/2022-01-24_Fuvar/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace _2022_01_24_Fuvar
 {
@@ -10,6 +11,12 @@ namespace _2022_01_24_Fuvar
     {
         static void Main(string[] args)
         {
+            List<Fuvar> fuvarok = new List<Fuvar>();
+            foreach (var sor in File.ReadAllLines("fuvar.csv").Skip(1))
+            {
+                fuvarok.Add(new Fuvar(sor));
+            }
+            Console.WriteLine($"3. feladat: {fuvarok.Count} fuvar");
         }
     }
 }
