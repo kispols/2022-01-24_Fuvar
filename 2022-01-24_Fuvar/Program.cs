9ad49e68ae7b3a90c03c0955bb0cec63e886ea00
kispols <kispal.attila0106@gmail.com>
2022-01-24 13:57:12 +0100

    Update Program.cs

--- 2022-01-24_Fuvar/Program.cs
+++ 2022-01-24_Fuvar/Program.cs
@@ -6,12 +6,27 @@ using System.Threading.Tasks;
 
 namespace _2022_01_24_Fuvar
 {
-    class Program
+    class Fuvar
     {
-        static void Main(string[] args)
+        public int TaxiID { get; set;}
+        public DateTime Indulás { get; set; }
+        public int Időtartam { get; set; }
+        public double Távolság { get; set; }
+        public double Viteldíj { get; set; }
+        public double Borravaló { get; set; }
+        public string FizetésMód { get; set; }
+
+        public Fuvar(string sor)
         {
-           
-            Console.ReadKey();
+            string[] t = sor.Split(';');
+            TaxiID = int.Parse(t[0]);
+            Indulás = DateTime.Parse(t[1]);
+            Időtartam = int.Parse(t[2]);
+            Távolság = double.Parse(t[3]);
+            Viteldíj = double.Parse(t[4]);
+            Borravaló = double.Parse(t[5]);
+            FizetésMód = t[6];
+
         }
     }
 }
